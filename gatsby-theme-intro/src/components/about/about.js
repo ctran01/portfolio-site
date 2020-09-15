6aea24347c143fcb9be51228a8bac507b11e4e39
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-09-15 17:25:15 -0500

    added resume and skills table

--- gatsby-theme-intro/src/components/about/about.js
+++ gatsby-theme-intro/src/components/about/about.js
@@ -8,7 +8,20 @@ const About = ({ about }) => (
     </h5>
     <div className="font-text text-sm pb-12 leading-normal whitespace-pre-line">
       {about}
+      <ul style={{display:"grid", gridTemplateColumns: "repeat(3,minmax(140px,200px))"}}>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• JavaScript</li>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• React</li>
+      <li className="font-text text-sm leading-normal whitespace-pre-line">• React Native</li>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• NodeJS</li>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• Express</li>
+      <li className="font-text text-sm leading-normal whitespace-pre-line">• Python</li>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• PostgreSQL</li>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• Flask</li>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• SQLAlchemy</li>
+      <li className="font-text text-sm  leading-normal whitespace-pre-line">• HTML/CSS</li>
+      </ul>
     </div>
+    
   </>
 )
 
--- gatsby-theme-intro/src/components/header/header.js
+++ gatsby-theme-intro/src/components/header/header.js
@@ -7,6 +7,12 @@ const Header = ({ initials }) => (
     <span className="inline-flex w-14 h-14 lg:mt-4 font-header font-bold text-xl justify-center items-center text-center text-front border-2 border-solid border-front rounded-full">
       {initials}
     </span>
+    <div className= "flex ">
+    <a
+    className="flex w-14 h-14 text-lead-text justify-center items-center leading-tight lg:w-auto lg:h-auto lg:px-6 lg:py-2 lg:rounded-lg lg:self-start lg:mt-4 hover:opacity-75 transition-opacity duration-150"
+    style={{marginRight:"15px"}} 
+    href="https://drive.google.com/file/d/1mOZ1DlptRM5Tx99TnGg2x0-OZK1H8zh0/view"
+    target="_blank">Resume</a>
     <a
       className="flex w-14 h-14 font-header font-semibold px-2 bg-lead rounded-full text-lead-text justify-center items-center leading-tight lg:w-auto lg:h-auto lg:px-6 lg:py-2 lg:rounded-lg lg:self-start lg:mt-4 hover:opacity-75 transition-opacity duration-150"
       href="#contact"
@@ -14,7 +20,10 @@ const Header = ({ initials }) => (
       <FaEnvelope className="inline-block h-6 w-6 lg:hidden" />
       <span className="hidden lg:block">Contact me</span>
     </a>
+    
+    </div>
   </header>
+  
 )
 
 Header.propTypes = {
