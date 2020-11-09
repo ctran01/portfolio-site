1f5135932afbe41b8ffafde7519311ffccab17cd
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-11-09 16:03:56 -0600

    Added metadata

--- gatsby-theme-intro/gatsby-config.js
+++ gatsby-theme-intro/gatsby-config.js
@@ -4,37 +4,40 @@
 //   showThemeLogo = true,
 //   theme = "dark-blue",
 // }) => {
-  module.exports={
-    siteMetadata: {
-      description: "Portfolio Site for Chris Tran",
-      locale: "en",
-      showThemeLogo : false,
-      title: "Chris Tran | Software Engineer",
+
+module.exports = {
+  siteMetadata: {
+    description: "Portfolio Site for Chris Tran",
+    locale: "en",
+    showThemeLogo: false,
+    title: "Chris Tran | Software Engineer",
+    author: "Chris Tran",
+    image: "./src/themes/chris.png",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss")(require("./tailwind.config")("dark-blue")),
+          require("postcss-input-range"),
+          require("autoprefixer"),
+        ],
+      },
     },
-    plugins: [
-      {
-        resolve: `gatsby-plugin-postcss`,
-        options: {
-          postCssPlugins: [
-            require("tailwindcss")(require("./tailwind.config")("dark-blue")),
-            require("postcss-input-range"),
-            require("autoprefixer"),
-          ],
-        },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: "content/",
       },
-      `gatsby-plugin-react-helmet`,
-      `gatsby-transformer-yaml`,
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: "content/",
-        },
-      },
-      {
-        resolve: "gatsby-plugin-react-svg",
-      },
-      `gatsby-transformer-sharp`,
-      `gatsby-plugin-sharp`,
-    ],
-  }
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
 // }
